bc3b7d24b62bccf1e448e90376827cb2518c0617
Shang Yi Ku <kushangyi2001@gmail.com>
2021-04-26 21:31:29 +0800

    Added search for friends, users functionality

--- components/main_page/connect/friends.js
+++ components/main_page/connect/friends.js
@@ -1,11 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { Icon } from "react-native-elements";
+import { FlatList, TouchableOpacity, TextInput } from 'react-native-gesture-handler'
 import firestore from "@react-native-firebase/firestore"
 import { DataContext } from '../../../context/dataContext'
 
 const friends = (props) => {
     const [friendList, setFriendList] = useState([])
+    const [addUserList, setAddUserList] = useState([])
+    const [filterUserList, setFilterUserList] = useState([])
+    const [searchUser, setSearchUser] = useState("")
+    const [searchFriend, setSearchFriend] = useState("")
     const { user, setUser } = useContext(DataContext)
 
     const ItemSeparatorView = () => {
@@ -26,6 +31,30 @@ const friends = (props) => {
         props.navigation.navigate("chat", { otherUID: friendUID })
     }
 
+    const updateFriendsInFirestore = (myNewFriends, friendUID, theirNewFriends) => {
+        firestore().collection('Users').doc(user.uid).update({ friends: myNewFriends.map(x => x._data.uid) })
+        firestore().collection('Users').doc(friendUID).update({ friends: theirNewFriends })
+    }
+
+    const changeFriendState = (friend) => {
+        if (friend._data.friends.includes(user.uid)) {
+            //Is already friend, remove
+            const newList = [...friendList]
+            const index1 = newList.findIndex(x => x._data.uid === friend._data.uid)
+            if (index1 > -1) newList.splice(index1, 1)
+            setFriendList(newList)
+            const index2 = friend._data.friends.findIndex(x => x === user.uid)
+            if (index2 > -1) friend._data.friends.splice(index2, 1)
+            updateFriendsInFirestore(newList, friend._data.uid, friend._data.friends)
+        } else {
+            //Not friend yet, add
+            const newList = [...friendList, friend]
+            setFriendList(newList)
+            friend._data.friends.push(user.uid)
+            updateFriendsInFirestore(newList, friend._data.uid, friend._data.friends)
+        }
+    }
+
     const renderName = (item) => {
         return (
             <TouchableOpacity
@@ -36,23 +65,62 @@ const friends = (props) => {
         )
     }
 
+    const renderAddFriend = (item) => {
+        return (
+            <TouchableOpacity
+                style={{ ...styles.friendButton, flexDirection: "row" }}
+                onPress={() => changeFriendState(item)}>
+                <Text style={{ fontSize: 25, width: "80%", marginLeft: "5%" }}>{item._data.name}</Text>
+                <Icon name={item._data.friends.includes(user.uid) ? "person-add-disabled" : "person-add"} type="MaterialIcons" color="turquoise" size={40}
+                />
+            </TouchableOpacity>
+        )
+    }
+
     useEffect(() => {
         const getUsersFromFirestore = async () => {
             let users = await firestore()
                 .collection('Users')
                 .where("uid", "!=", user.uid)
                 .get()
-            setFriendList(users._docs)
+            setAddUserList(users._docs)
         }
         getUsersFromFirestore()
+
+        const getFriendsFromFirestore = async () => {
+            let friends = await firestore()
+                .collection('Users')
+                .where("friends", "array-contains", user.uid)
+                .get()
+            setFriendList(friends._docs)
+        }
+        getFriendsFromFirestore()
     }, [])
 
     return (
         <View style={styles.container}>
-            <Text style={styles.chatWith}>Chat with:</Text>
+            <Text style={styles.chatWith}>Add friends:</Text>
+            <TextInput
+                style={styles.search}
+                placeholder="Search name..."
+                onChangeText={(text) => setSearchUser(text.toLowerCase())}
+            />
             <FlatList
                 style={styles.friendList}
-                data={friendList}
+                data={searchUser ? addUserList.filter(x => x._data.name.toLowerCase().includes(searchUser)) : addUserList}
+                ItemSeparatorComponent={ItemSeparatorView}
+                keyExtractor={(item) => String(item._data.uid)}
+                renderItem={({ item }) => renderAddFriend(item)}>
+            </FlatList>
+            <Text style={styles.chatWith}>Chat with:</Text>
+            <TextInput
+                style={styles.search}
+                placeholder="Search friend..."
+                onChangeText={(text) => setSearchFriend(text.toLowerCase())}
+            />
+            <FlatList
+                style={styles.friendList}
+                data={searchFriend ? friendList.filter(x => x._data.name.toLowerCase().includes(searchFriend)) : friendList}
                 ItemSeparatorComponent={ItemSeparatorView}
                 keyExtractor={(item) => String(item._data.uid)}
                 renderItem={({ item }) => renderName(item)}>
@@ -72,14 +140,25 @@ const styles = StyleSheet.create({
     },
     chatWith: {
         fontSize: 30,
-        marginVertical: 5
+        marginTop: 10
     },
     friendList: {
         flex: 1,
         width: "100%",
-        marginTop: 20
+        height: "45%"
     },
     friendButton: {
         paddingVertical: 15,
-    }
+    },
+    search: {
+        fontSize: 25,
+        padding: 10,
+        backgroundColor: "white",
+        color: "turquoise",
+        borderColor: "turquoise",
+        margin: 15,
+        borderWidth: 2,
+        width: "70%",
+        borderRadius: 20
+    },
 })
